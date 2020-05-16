e39718f1579e61d7175c0e190b9640f885caa35e
MatiFreyre <mfreyre@gmail.com>
2020-05-16 09:53:38 -0300

    Agregado de tests de puntos 2 y 3

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,8 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+programaAvancePC :: Programa
+programaAvancePC = nop . nop . nop
 
 programaSuma :: Programa
 programaSuma = add' . lodv 22 . swap . lodv 10
@@ -9,9 +11,24 @@ programaSuma = add' . lodv 22 . swap . lodv 10
 programaDivision :: Programa
 programaDivision = division' . lod 1 . swap . lod 2 . str 2 0 . str 1 2
 
+fp20 = xt8088 {
+  acumuladorA = 7,
+  acumuladorB = 24
+}
+
 main :: IO ()
 main = hspec $ do
   describe "Tests entrega 1 TP" $ do
+    it "Programa con 3 NOP sólo cambia el PC a 3" $ do
+      programaAvancePC  xt8088 `shouldBe` xt8088 { programCounter = 3 }
+    it "LODV cambia acumulador A" $ do
+      (acumuladorA . lodv 5) xt8088 `shouldBe` 5
+    it "LODV no cambia acumulador B" $ do
+      (acumuladorB . lodv 5) xt8088 `shouldBe` 0
+    it "SWAP deja en acumulador A el valor del B" $ do
+      (acumuladorA . swap) fp20 `shouldBe` 24
+    it "SWAP deja en acumulador B el valor del A" $ do
+      (acumuladorB . swap) fp20 `shouldBe` 7
     it "Programa suma 22 y 10 y acumulador A queda en 32" $ do
       (acumuladorA $ programaSuma xt8088) `shouldBe` 32
     it "Programa divide 2 por 0 y etiqueta queda con el error 'DIVISION BY ZERO'" $ do
